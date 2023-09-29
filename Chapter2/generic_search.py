85547c3c265538cd4afe2cbe20a6c529ce2bd6fb
Kyungwon Chun <ruddyscent@gmail.com>
2023-09-30 00:57:35 +0900

    Python 3.10.2 supports Protocol

--- Chapter2/generic_search.py
+++ Chapter2/generic_search.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 from __future__ import annotations
 from typing import TypeVar, Iterable, Sequence, Generic, List, Callable, Set, Deque, Dict, Any, Optional
-from typing_extensions import Protocol
+from typing import Protocol
 from heapq import heappush, heappop
 
 T = TypeVar('T')
